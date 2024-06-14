a87e20cc998e5cf11dc3c51e56801c2955cd6619
Lucas Luan dos Santos <lucas.l.santos0204@gmail.com>
2024-06-14 20:45:17 -0300

    tela principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TelaAbertutaComponent } from './tela-abertuta/tela-abertuta.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TelaAbertutaComponent],
+  imports: [RouterOutlet, TelaAbertutaComponent, LandingPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
